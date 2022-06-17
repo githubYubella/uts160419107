b0527f3fab640aa2eed4a9a2c439c780eb0fe4d9
patricklime <s160419092@student.ubaya.ac.id>
2022-06-17 07:40:37 +0700

    interface added

---
+++ app/src/main/java/id/ac/ubaya/a160419107_ubayakost/view/Interface.kt
@@ -0,0 +1,19 @@
+package id.ac.ubaya.a160419107_ubayakost.view
+
+import android.view.View
+
+interface ButtonDetailClickListener{
+    fun onButtonDetailClick(v: View)
+}
+
+interface ButtonPesanClickListener{
+    fun onButtonPesanClick(v: View)
+}
+
+interface RefreshClickListener{
+    fun onRefreshClick(v: View)
+}
+
+interface DoneClickListener{
+    fun onDoneButtonClick(v: View)
+}
