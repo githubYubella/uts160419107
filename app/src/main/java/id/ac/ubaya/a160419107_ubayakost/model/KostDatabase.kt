60c8351366cc74e766019ad5a1a98ed768fe8d9b
patricklime <s160419092@student.ubaya.ac.id>
2022-06-27 10:53:28 +0700

    room database

---
+++ app/src/main/java/id/ac/ubaya/a160419107_ubayakost/model/KostDatabase.kt
@@ -0,0 +1,47 @@
+package id.ac.ubaya.a160419107_ubayakost.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+//Kalau ditambahkan migration jangan lupa versinya dinaikkan
+//contoh versinya menjadi 2 dan untuk menambahkan tabel lagi seperti berikut
+//@Database(entities = [KostUbaya::class, PenggunaKost::class], version =  2)
+@Database(entities = arrayOf(KostUbaya::class), version = 1)
+abstract class KostDatabase:RoomDatabase() {
+    abstract fun kostDao(): KostDao
+
+    companion object{
+        @Volatile private var instance: KostDatabase ?= null
+        private val Lock = Any()
+
+//        buat database
+        private fun buildDatabase(context: Context)= Room.databaseBuilder(
+            context.applicationContext, KostDatabase::class.java, "kostdb"
+        ).build()
+
+//  Kalau mau ditambahkan migration
+//        private fun buildDatabase(context: Context) =
+//            Room.databaseBuilder(
+//                context.applicationContext,
+//                TodoDatabase::class.java,
+//                "kostdb"
+//            ).addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build()
+
+
+        operator fun invoke(context: Context){
+            if(instance != null){
+                synchronized(Lock){
+                    instance ?: buildDatabase(context).also {
+                        instance = it
+                    }
+                }
+
+            }
+        }
+
+
+    }
+
+}
--- app/src/main/java/id/ac/ubaya/a160419107_ubayakost/util/util.kt
+++ app/src/main/java/id/ac/ubaya/a160419107_ubayakost/util/util.kt
@@ -1,13 +1,20 @@
 package id.ac.ubaya.a160419107_ubayakost.util
 
+import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
+import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import id.ac.ubaya.a160419107_ubayakost.R
+import id.ac.ubaya.a160419107_ubayakost.model.KostDatabase
 import java.lang.Exception
+
+val db_name="kostdb"
 
 fun ImageView.loadImage(url:String?, progressBar: ProgressBar){
     Picasso.get()
@@ -27,6 +34,37 @@ fun ImageView.loadImage(url:String?, progressBar: ProgressBar){
 
         })
 }
+
+fun buildDatabase(context: Context)= Room.databaseBuilder(
+    context.applicationContext, KostDatabase::class.java, db_name
+).build()
+//builDatabase ini juga perlu ditambahkan migration
+//        private fun buildDatabase(context: Context) =
+//            Room.databaseBuilder(
+//                context.applicationContext,
+//                TodoDatabase::class.java,
+//                "kostdb"
+//            ).addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build()
+
+//contoh ingin menambahkan migration berupa pembuatan tabel pengguna
+//val MIGRATION_1_2 =object : Migration(1,2){
+//    override fun migrate(database: SupportSQLiteDatabase) {
+//        database.execSQL(
+//            "CREATE TABLE penggunakost (id INTEGER PRIMARY KEY NOT NULL, nama TEXT, email TEXT," +
+//                    " telepon TEXT, username TEXT, password TEXT, peran TEXT)"
+//        )
+//
+//    }
+//}
+//Tambah kolom
+//val MIGRATION_2_3 =object : Migration(2,3){
+//    override fun migrate(database: SupportSQLiteDatabase) {
+//        database.execSQL(
+//            "ALTER TABLE todo ADD COLUMN priority INTEGER DEFAULT 3 not null"
+//        )
+//
+//    }
+//}
 
 @BindingAdapter("android:imageUrl", "android:progreesBar")
 fun loadPhotoUrl(view: ImageView,url:String?, progressBar:ProgressBar){
